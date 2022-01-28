540a0068649bc3dd271b6e86151c69f4cf13a5e7
bosikova <katerinabosikova93@gmail.com>
2022-01-28 21:07:00 +0300

    Edits

--- exam/js/all-tasks.js
+++ exam/js/all-tasks.js
@@ -21,35 +21,49 @@ console.log(getTaskFromLS);
 // если пользователь ввел задачи
 if (getTaskFromLS.length>0) {
     let taskList = document.createElement("div");
+      taskList.classList.add("tasks-container")
     // перебираем массив с объектами
     for (let i=0; i<getTaskFromLS.length; i++) {
-        let outputList = document.createElement("div");
+        let oneTask = document.createElement("div");
+        oneTask.classList.add("task-container");
         let taskName = document.createElement("h2");
         taskName.innerText = getTaskFromLS[i].title;
         let taskDescription = document.createElement("p");
         taskDescription.innerText = getTaskFromLS[i].description;
         let date = document.createElement("p");
         date.innerText = `Выполнить к: ${getTaskFromLS[i].date}`;
-        taskList.append(outputList);
-        taskList.append(taskName);
-        taskList.append(taskDescription);
-        taskList.append(date);
+        oneTask.append(taskName);
+        oneTask.append(taskDescription);
+        oneTask.append(date);
+        taskList.append(oneTask);
+
+        oneTask.addEventListener("click", function(){
+            this.classList.toggle("selected")
+        })
     }
-    document.body.append(taskList);
-};
-
-
-// если пользователь НЕ ввел задачи
-if (getTaskFromLS.length===0) {
-    let emptyTaskList = document.createElement("div");
-    emptyTaskList.innerText = "Список задач пуст";
+   document.body.append(taskList);
 };
 
 
 
+let btn_delete = document.querySelector('.delete-task-from-ls');
 
-//создаем пустой массив для удаленных задач
-let deleteTaskFromLS = [];
+btn_delete.addEventListener("click", function() {
+    let arr = document.querySelectorAll('.selected');
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].remove();
+    }
+    let new_arr = [];
+    arr = document.querySelectorAll('.task-container');
+    for (let i = 0; i < arr.length; i++) {
+        new_arr.push({
+            title: arr[i].childNodes[0].innerText,
+            description: arr[i].childNodes[1].innerText,
+            date: arr[i].childNodes[2].innerText,
+        });
+    }
+    localStorage.setItem("tasks", JSON.stringify(new_arr));
+});
 
 
 // полная очистка хранилища
